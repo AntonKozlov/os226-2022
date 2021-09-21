6a8cdc6c1168386c6d2dce8a1becccbf9324b2e0
Anton Kozlov <akozlov@azul.com>
2021-09-21 23:04:47 +0300

    HW2 solution

--- pool.h
+++ pool.h
@@ -2,11 +2,24 @@
 
 #include "util.h"
 
-struct pool {
+struct pool_free_block {
+	struct pool_free_block *next;
+};
 
+struct pool {
+	char *mem;
+	unsigned long membsz;
+	char *freestart;
+	char *freeend;
+	struct pool_free_block *freehead;
 };
 
 #define POOL_INITIALIZER(_mem, _nmemb, _membsz) { \
+	.mem = (char*)(_mem), \
+	.membsz	= (_membsz), \
+	.freehead = NULL, \
+	.freestart = (char*)(_mem), \
+	.freeend = (char*)(_mem) + (_nmemb) * (_membsz), \
 }
 
 #define POOL_INITIALIZER_ARRAY(_array) \
