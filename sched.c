0a273dc9a005e1a824ee4c9f7b658ccb042fb417
Anton Kozlov <akozlov@azul.com>
2021-10-13 01:05:33 +0300

    HW4 solution

--- sched.c
+++ sched.c
@@ -5,25 +5,130 @@
 #include "sched.h"
 #include "pool.h"
 
+struct task {
+	void (*entry)(void *as);
+	void *as;
+	int priority;
+	int deadline;
+
+	// timeout support
+	int waketime;
+
+	// policy support
+	struct task *next;
+};
+
 static int time;
+
+static struct task *current;
+static struct task *runq;
+static struct task *waitq;
+
+static struct task *pendingq;
+static struct task *lastpending;
+
+static int (*policy_cmp)(struct task *t1, struct task *t2);
+
+static struct task taskarray[16];
+static struct pool taskpool = POOL_INITIALIZER_ARRAY(taskarray);
+
+static void policy_run(struct task *t) {
+	struct task **c = &runq;
+
+	while (*c && policy_cmp(*c, t) <= 0) {
+		c = &(*c)->next;
+	}
+	t->next = *c;
+	*c = t;
+}
 
 void sched_new(void (*entrypoint)(void *aspace),
 		void *aspace,
 		int priority,
 		int deadline) {
 
+	struct task *t = pool_alloc(&taskpool);;
+	t->entry = entrypoint;
+	t->as = aspace;
+	t->priority = priority;
+	t->deadline = 0 < deadline ? deadline : INT_MAX;
+	t->next = NULL;
+
+	if (!lastpending) {
+		lastpending = t;
+		pendingq = t;
+	} else {
+		lastpending->next = t;
+		lastpending = t;
+	}
 }
 
 void sched_cont(void (*entrypoint)(void *aspace),
 		void *aspace,
 		int timeout) {
 
+	if (current->next != current) {
+		fprintf(stderr, "Mulitiple sched_cont\n");
+		return;
+	}
+
+	if (!timeout) {
+		policy_run(current);
+		return;
+	}
+
+	current->waketime = time + timeout;
+
+	struct task **c = &waitq;
+	while (*c && (*c)->waketime < current->waketime) {
+		c = &(*c)->next;
+	}
+	current->next = *c;
+	*c = current;
 }
 
 void sched_time_elapsed(unsigned amount) {
 	time += amount;
 
+	while (waitq && waitq->waketime <= time) {
+		struct task *t = waitq;
+		waitq = waitq->next;
+		policy_run(t);
+	}
+}
+
+static int fifo_cmp(struct task *t1, struct task *t2) {
+	return -1;
+}
+
+static int prio_cmp(struct task *t1, struct task *t2) {
+	return t2->priority - t1->priority;
+}
+
+static int deadline_cmp(struct task *t1, struct task *t2) {
+	int d = t1->deadline - t2->deadline;
+	if (d) {
+		return d;
+	}
+	return prio_cmp(t1, t2);
 }
 
 void sched_run(enum policy policy) {
+	int (*policies[])(struct task *t1, struct task *t2) = { fifo_cmp, prio_cmp, deadline_cmp };
+	policy_cmp = policies[policy];
+
+	struct task *t = pendingq;
+	while (t) {
+		struct task *next = t->next;
+		policy_run(t);
+		t = next;
+	}
+
+	while (runq) {
+		current = runq;
+		runq = current->next;
+		current->next = current;
+
+		current->entry(current->as);
+	}
 }
