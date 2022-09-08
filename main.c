064860bb2b168388cc66d7f20880f306291fb280
Startsev Matvey <89902951+Tozarin@users.noreply.github.com>
2022-09-08 13:39:43 +0300

    Implemented main

--- main.c
+++ main.c
@@ -1,17 +1,98 @@
-
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define SIZE_OF_INPUT 80
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int echo(int argc, char *argv[]) {
-	for (int i = 1; i < argc; ++i) {
-		printf("%s%c", argv[i], i == argc - 1 ? '\n' : ' ');
-	}
-	return argc - 1;
+
+//----------global----------
+int _ret_code = 0;
+//--------------------------
+
+int echo(int argc, char* argv[]);
+int retcode(int argc, char* argv[]);
+
+void interpret(char** parsed_input, int size_of_parsed_input)
+{
+    char* args[SIZE_OF_INPUT];
+    int size_of_args = 0;
+
+    for (int i = 0; i < size_of_parsed_input; ++i)
+    {
+        char* token = parsed_input[i];
+        args[size_of_args++] = token;
+
+        if (!strcmp(token, "retcode"))
+        {
+            retcode(size_of_args, args);
+        }
+        else if (!strcmp(token, "echo"))
+        {
+            while (1)
+            {
+                if (i + 1 >= size_of_parsed_input)
+                {
+                    break;
+                }
+
+                token = parsed_input[++i];
+
+                if (!strcmp(token, "echo") || !strcmp(token, "retcode"))
+                {
+                    i--;
+                    break;
+                }
+
+                args[size_of_args++] = token;
+            }
+
+            echo(size_of_args, args);
+        }
+
+        size_of_args = 0;
+    }
 }
 
-int retcode(int argc, char *argv[]) {
+int echo(int argc, char* argv[])
+{
+    for (int i = 1; i < argc; ++i)
+    {
+        printf("%s%c", argv[i], i == argc - 1 ? '\n' : ' ');
+    }
+    _ret_code = argc - 1;
+
+    return _ret_code;
 }
 
-int main(int argc, char *argv[]) {
-	// IMPL ME
-	return 0;
+int retcode(int argc, char* argv[])
+{
+    printf("%d\n", _ret_code);
+
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    char* input[SIZE_OF_INPUT];
+    char* sep = " ;\n";
+
+    while (fgets(input, SIZE_OF_INPUT, stdin) != NULL)
+    {
+        char* parsed_input[80];
+        int size_of_parsed_input = 0;
+
+        //printf("%s\n", input);
+
+        char* token = strtok(input, sep);
+
+        while (token!= NULL)
+        {
+            parsed_input[size_of_parsed_input++] = token;
+            token = strtok(NULL, sep);
+        }
+
+        interpret(parsed_input, size_of_parsed_input);
+    }
+
+    return 0;
 }
