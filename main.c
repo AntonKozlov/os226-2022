c8acbda3338d1e7c0d0bf9e88e7936d3a0fe8bfe
Ilya Pankratov <i.pankratov.main@gmail.com>
2022-09-10 14:46:19 +0300

    interpete function was implemented

--- main.c
+++ main.c
@@ -3,7 +3,11 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define STANDARD_INPUT_SIZE 1024
 #define STANDARD_BUFFER 128
+
+/// global variable for storing a retcode from functions
+int _retcode = 0;
 
 int echo(int argc, char *argv[]) {
 	for (int i = 1; i < argc; ++i) {
@@ -29,6 +33,37 @@ char** parse(char* input, int *output_argument_number) {
 	*output_argument_number = tokens_found - 1;
 
 	return parsed_input;
+}
+
+void interprete(int argc, char **argv) {
+	int arguments_count = 0;
+	char* arguments[STANDARD_INPUT_SIZE];
+
+	for (int counter = 0; counter < argc; counter++)
+	{
+		char* token = argv[counter];
+
+		if (!strcmp(token, "retcode")) {
+			_retcode = retcode(arguments_count, arguments);
+		}
+		else if (!strcmp(token, "echo")) {
+			arguments_count = 0;
+			arguments[arguments_count++] = token;
+
+			while ((counter + 1 != argc) && ((strcmp(argv[++counter], "retcode")) && (strcmp (argv[counter], "echo"))))
+			{
+				arguments[arguments_count++] = argv[counter];
+			}
+
+			_retcode = echo(arguments_count, arguments);
+			counter--;
+		}
+		else {
+			break;
+		}
+	}
+
+	return;
 }
 
 int main(int argc, char *argv[]) {
