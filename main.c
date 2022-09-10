71da94741a7372269d15859f2f80deb18d0ec36f
Ilya Pankratov <i.pankratov.main@gmail.com>
2022-09-10 14:44:50 +0300

    parse function was implemented

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define STANDARD_BUFFER 128
 
 int echo(int argc, char *argv[]) {
 	for (int i = 1; i < argc; ++i) {
@@ -9,6 +13,22 @@ int echo(int argc, char *argv[]) {
 }
 
 int retcode(int argc, char *argv[]) {
+}
+
+char** parse(char* input, int *output_argument_number) {
+	char* command_separators = " \n;";
+	char** parsed_input = (char**)malloc(sizeof(char*) * STANDARD_BUFFER);
+	int tokens_found = 0;
+
+	parsed_input[tokens_found++] = strtok(input, command_separators);
+
+	while (parsed_input[tokens_found - 1] != NULL) {
+		parsed_input[tokens_found++] = strtok(NULL, command_separators);;
+	}
+
+	*output_argument_number = tokens_found - 1;
+
+	return parsed_input;
 }
 
 int main(int argc, char *argv[]) {
