afe547fa1a3c9237e03e7166f5c704bc2b270aad
ch3zych3z <chester-vyacheslav@yandex.ru>
2022-09-11 15:27:21 +0300

    Added parser and interpreter for echo and retcode

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
-
 #include <stdio.h>
+#include <string.h>
+
+const int MAX_LEN = 1024;
+const char *STATEMENT_SEPS = ";";
+const char *TOKEN_SEPS = " ";
+
+
+int lst_retcode = 0;
 
 int echo(int argc, char *argv[]) {
 	for (int i = 1; i < argc; ++i) {
@@ -9,9 +16,58 @@ int echo(int argc, char *argv[]) {
 }
 
 int retcode(int argc, char *argv[]) {
+	printf("%d\n", lst_retcode);
+	return 0;
+}
+
+void clean_line(char* str) {
+	while (str && *str != '\0' && *str != '\n')
+		str++;
+	if (str && *str == '\n')
+		*str = '\0';
+}
+
+int tokenize(char* statement, char** tokens, int size) {
+	char *token, *brkb;
+	int cnt = 0;
+	for (token = strtok_r(statement, TOKEN_SEPS, &brkb);
+		 token && cnt < size;
+		 token = strtok_r(NULL, TOKEN_SEPS, &brkb), cnt++) {
+		*tokens = token;
+		tokens++;
+	}
+	return cnt;
+}
+
+int execute_statement(char* statement) {
+	char* tokens[MAX_LEN];
+	int argc = tokenize(statement, tokens, MAX_LEN);
+
+	int (*command)(int, char**) = NULL;
+
+	if (argc > 0) {
+		if (strcmp("echo", tokens[0]) == 0)
+			command = echo;
+		if (strcmp("retcode", tokens[0]) == 0)
+			command = retcode;
+	}
+
+	if (command == NULL)
+		return -1;
+	return command(argc, tokens);
 }
 
 int main(int argc, char *argv[]) {
-	// IMPL ME
+	char line[MAX_LEN];
+	char *statement, *brkt;
+
+	while (fgets(line, MAX_LEN, stdin)) {
+		clean_line(line);
+		for (statement = strtok_r(line, STATEMENT_SEPS, &brkt);
+			 statement;
+			 statement = strtok_r(NULL, STATEMENT_SEPS, &brkt)) {
+			lst_retcode = execute_statement(statement);
+		}
+	}
 	return 0;
 }
