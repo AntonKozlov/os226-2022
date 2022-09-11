a0a50d5911023c606e402efe2aa1eabe1d465bf2
Oleg Bogomolov <smmac072@gmail.com>
2022-09-11 16:41:02 +0300

    echo and retcode elements in the command parser

--- main.c
+++ main.c
@@ -1,17 +1,74 @@
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int echo(int argc, char *argv[]) {
-	for (int i = 1; i < argc; ++i) {
+#define MAX_INPUT_SIZE 512
+#define MAX_ARGUMENT_SIZE 128
+#define MAX_ARGUMENT_COUNT 32
+
+
+const char CONST_RETCODE_KEYWORD[] = "retcode";
+const char CONST_ECHO_KEYWORD[] = "echo";
+const char CONST_PARSE_DELIMETERS[] = " ;\n";
+int LASTRETCODE = 0;
+
+int echo(int argc, char *argv[]) 
+{
+	for (int i = 1; i < argc; ++i)
 		printf("%s%c", argv[i], i == argc - 1 ? '\n' : ' ');
-	}
 	return argc - 1;
 }
 
-int retcode(int argc, char *argv[]) {
+int retcode() 
+{
+	printf("%d\n", LASTRETCODE);
+	return 0;
+}
+void parse(char* InputString)
+{
+	char* ProcessedPart = strtok(InputString, CONST_PARSE_DELIMETERS);
+	int Elements = 0;
+	char SplitInput[MAX_ARGUMENT_COUNT][MAX_ARGUMENT_SIZE];
+	while (ProcessedPart) 
+	{
+		strcpy(SplitInput[Elements++], ProcessedPart);
+		ProcessedPart = strtok(NULL, CONST_PARSE_DELIMETERS);
+	}
+
+	for (int i = 0; i < Elements; i++)
+	{
+		if (!strcmp(SplitInput[i], CONST_RETCODE_KEYWORD))
+			LASTRETCODE = retcode();
+		if (!strcmp(SplitInput[i], CONST_ECHO_KEYWORD))
+		{
+			int argc = 1;
+			char *argv[MAX_ARGUMENT_COUNT];
+			for (int alloc = 1; alloc < MAX_ARGUMENT_COUNT; alloc++)
+				argv[alloc] = (char*)malloc(MAX_ARGUMENT_SIZE);
+
+			for (int j = i + 1; j < Elements; j++)
+			{
+				if (!strcmp(SplitInput[j], CONST_ECHO_KEYWORD) || !strcmp(SplitInput[j], CONST_RETCODE_KEYWORD))
+					break;
+				else 
+					strcpy(argv[argc++], SplitInput[j]);
+			}
+			LASTRETCODE = echo(argc, argv);
+			
+
+			free(*argv);
+		}
+	}
+	free(ProcessedPart);
 }
 
-int main(int argc, char *argv[]) {
-	// IMPL ME
+int main(int argc, char *argv[]) 
+{
+	char InputString[MAX_INPUT_SIZE];
+	while (fgets(InputString, MAX_INPUT_SIZE, stdin))
+	{
+		parse(InputString);
+	}
 	return 0;
 }
