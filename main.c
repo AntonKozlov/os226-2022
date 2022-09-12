3dc6453d9d14fde6203bfb4bf48cf378ca5c0d7c
Purpletta <101121914+Purpletta@users.noreply.github.com>
2022-09-12 11:33:09 +0300

    Update main.c
    "edited retcode and echo"

--- main.c
+++ main.c
@@ -1,17 +1,74 @@
-
 #include <stdio.h>
+#include <string.h>
 
-int echo(int argc, char *argv[]) {
+#define BUFFER_SIZE 256
+#define ARGS_BUFFER_SIZE 256
+
+int last_retcode = 0;
+
+int echo(int argc, char* argv[]) {
 	for (int i = 1; i < argc; ++i) {
 		printf("%s%c", argv[i], i == argc - 1 ? '\n' : ' ');
 	}
 	return argc - 1;
 }
 
-int retcode(int argc, char *argv[]) {
+int retcode(int argc, char* argv[]) {
+	printf("%d\n", last_retcode);
+	return 0;
 }
 
-int main(int argc, char *argv[]) {
-	// IMPL ME
+
+
+void process(char* input)
+{
+	char* cmd_divs = ";\n";
+	char* args_divs = " \n";
+
+	
+	char* first_ptr;
+	char* second_ptr;
+	char* args_str = strtok_r(input, cmd_divs, &first_ptr);
+
+	
+	while (args_str != NULL)
+	{
+		char* arg = strtok_r(args_str, args_divs, &second_ptr);
+
+		int args_count = 0;
+		char* args[ARGS_BUFFER_SIZE];
+
+		
+		while (arg != NULL)
+		{
+			args[args_count] = arg;
+			args_count++;
+
+			arg = strtok_r(NULL, args_divs, &second_ptr);
+		}
+
+		
+		if (strcmp(args[0], "echo") == 0)
+		{
+			last_retcode = echo(args_count, args);
+		}
+		else if (strcmp(args[0], "retcode") == 0)
+		{
+			last_retcode = retcode(args_count, args);
+		}
+
+		args_str = strtok_r(NULL, cmd_divs, &first_ptr);
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	char input[BUFFER_SIZE];
+
+	while (fgets(input, sizeof(input), stdin) != NULL)
+	{
+		process(input);
+	}
+
 	return 0;
 }
