857cad32b2f3e42135c76d5974424d440ba31d92
Maxim <drumovm@DESKTOP-A4OD4EV.campus-ws.pu.ru>
2022-09-12 03:20:32 +0300

    Add implementation of echo and retcode

--- main.c
+++ main.c
@@ -1,17 +1,79 @@
-
 #include <stdio.h>
+#include <string.h>
 
-int echo(int argc, char *argv[]) {
-	for (int i = 1; i < argc; ++i) {
+#define MAX_INPUT_LENGTH 512
+#define MAX_NUMBER_ARGS 64
+
+int RETCODE = 0;
+
+int echo(int argc, char *argv[]) 
+{
+	for (int i = 1; i < argc; ++i) 
+	{
 		printf("%s%c", argv[i], i == argc - 1 ? '\n' : ' ');
 	}
 	return argc - 1;
 }
 
-int retcode(int argc, char *argv[]) {
+int retcode(int argc, char *argv[]) 
+{
+	printf("%d\n", RETCODE);
+	return 0;
 }
 
-int main(int argc, char *argv[]) {
-	// IMPL ME
+void runCommand(int count, char* args[])
+{
+	if(strncmp(args[0], "echo", 4) == 0)
+	{
+		RETCODE = echo(count, args);
+	}
+	else if(strncmp(args[0], "retcode", 7) == 0)
+	{
+		RETCODE = retcode(count, args);
+	}
+}
+
+void execute(char* input)
+{
+	char* args_str;
+	char* arg;
+
+	char* args_ptr;
+	char* arg_ptr;
+
+	char* args[MAX_NUMBER_ARGS];
+
+	int counter = 0;
+
+	args_str = strtok_r(input, ";\n", &args_ptr);
+
+	while (args_str)
+	{
+		arg = strtok_r(args_str, " \n", &arg_ptr);
+
+		while (arg != NULL)
+		{
+			args[counter] = arg;
+			arg = strtok_r(NULL, " \n", &arg_ptr);
+
+			counter++;
+		}
+
+		runCommand(counter, args);
+
+		counter = 0;
+		args_str = strtok_r(NULL, ";\n", &args_ptr);
+	}
+}
+
+int main(int argc, char *argv[]) 
+{
+	char input[MAX_INPUT_LENGTH];
+
+	while (fgets(input, MAX_INPUT_LENGTH, stdin) != NULL)
+	{
+		execute(input);
+	}
+
 	return 0;
 }
