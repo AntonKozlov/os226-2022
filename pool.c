f8bfce5cbae40f100ffb5ff880d8c51d5f50db1b
SuldinVyacheslav <suldin_slava.03@mail.ru>
2022-09-17 01:33:04 +0300

    initial commit of HW2

--- pool.c
+++ pool.c
@@ -1,14 +1,38 @@
-
 #include <stddef.h>
 
 #include "pool.h"
 
 void pool_init(struct pool *p, void *mem, unsigned long nmemb, unsigned long membsz) {
+    p->mem = mem;
+    p->membsz = membsz;
+    p->freestart = (char*)mem;
+    p->freeend = (char*)mem + nmemb * membsz;
+    p->free = NULL;
 }
 
 void *pool_alloc(struct pool *p) {
-	return NULL;
+    struct pool_free_block *fb = p->free;
+    if (fb) {
+        p->free = fb->next;
+        return fb;
+    }
+
+    if (p->freestart < p->freeend) {
+        void *r = p->freestart;
+        p->freestart += p->membsz;
+        return r;
+    }
+
+    return NULL;
 }
 
 void pool_free(struct pool *p, void *ptr) {
+    if (ptr)
+    {
+        void *tmp_next = p->free;
+        p->free = ptr;
+        p->free->next = tmp_next;
+    }
+    else return;
+
 }
