48c547003f2abdee5684d7687283dd7d3b290754
Ivan Shurekov <ivan.shurenkov@yandex.com>
2022-09-18 00:18:51 +0300

    write pool allocator

--- pool.c
+++ pool.c
@@ -4,11 +4,31 @@
 #include "pool.h"
 
 void pool_init(struct pool *p, void *mem, unsigned long nmemb, unsigned long membsz) {
+	p->mem = mem;
+	p->membsz = membsz;
+	p->freestart = mem;
+	p->freeend = mem + nmemb * membsz;
+	p->free = NULL;
 }
 
 void *pool_alloc(struct pool *p) {
+	struct pool_free_block *fb = p->free;
+	if (fb) {
+		p->free = fb->next;
+		return fb;
+	}
+	if (p->freestart < p->freeend) {
+		void *r = p->freestart;
+		p->freestart += p->membsz;
+		return r;
+	}
 	return NULL;
 }
 
 void pool_free(struct pool *p, void *ptr) {
+	if (ptr) {
+		void *temp_ptr = p->free;
+		p->free = ptr;
+		(*p->free).next = temp_ptr;
+	}
 }
