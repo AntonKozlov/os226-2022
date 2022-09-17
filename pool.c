f81107981f1794d8563fdad7136c2bbc218268bd
Sayumiko <90759630+sayumikko@users.noreply.github.com>
2022-09-17 17:08:27 +0300

    Added solution for HW2

--- pool.c
+++ pool.c
@@ -1,14 +1,44 @@
-
 #include <stddef.h>
-
 #include "pool.h"
+#define ull unsigned long long
 
-void pool_init(struct pool *p, void *mem, unsigned long nmemb, unsigned long membsz) {
+void pool_init(struct pool* p, void* mem, unsigned long nmemb, unsigned long membsz)
+{
+
 }
 
-void *pool_alloc(struct pool *p) {
-	return NULL;
+void* pool_alloc(struct pool* p)
+{
+    if (p->freeblocks == 0)
+    {
+        return NULL;
+    }
+
+    // TODO уменьшать freeblocks
+    if (p->initialized_blocks < p->numblk)
+    {
+        ull* var = ((ull*)((ull)p->mem_begin + p->initialized_blocks * p->memblksz + p->memblksz));
+        *((ull*)((ull)p->mem_begin + p->initialized_blocks * p->memblksz)) = var;
+        p->initialized_blocks++;
+    }
+
+    if (p->freeblocks == 0)
+    {
+        p->ptr = NULL;
+        return NULL;
+    }
+    else
+    {
+        void* tmpptr = p->ptr;
+        p->ptr = (void*)(*(ull*)p->ptr);
+        p->freeblocks--;
+        return  tmpptr;
+    }
 }
 
-void pool_free(struct pool *p, void *ptr) {
+void pool_free(struct pool* p, void* ptr)
+{
+    *(unsigned long long*)ptr = (unsigned long long)p->ptr;
+    p->ptr = ptr;
+    p->freeblocks += 1;
 }
--- pool.h
+++ pool.h
@@ -1,19 +1,32 @@
 #pragma once
-
 #include "util.h"
 
-struct pool {
 
+struct pool
+{
+    void* mem_begin;
+    int freeblocks;
+    int numblk;
+    int memblksz;
+    void* ptr;
+    int initialized_blocks;
 };
 
-#define POOL_INITIALIZER(_mem, _nmemb, _membsz) { \
+#define POOL_INITIALIZER(_mem, _nmemb, _membsz) \
+{\
+    _mem,                                            \
+    _nmemb,\
+    _nmemb,\
+    _membsz,\
+    _mem,\
+    0,\
 }
 
 #define POOL_INITIALIZER_ARRAY(_array) \
-	POOL_INITIALIZER(_array, ARRAY_SIZE(_array), sizeof((_array)[0]));
+    POOL_INITIALIZER(_array, ARRAY_SIZE(_array), sizeof((_array)[0]));
 
-void pool_init(struct pool *p, void *mem, unsigned long nmemb, unsigned long membsz);
+void pool_init(struct pool* p, void* mem, unsigned long nmemb, unsigned long membsz);
 
-void *pool_alloc(struct pool *p);
+void* pool_alloc(struct pool* p);
 
-void pool_free(struct pool *p, void *ptr);
+void pool_free(struct pool* p, void* ptr);
