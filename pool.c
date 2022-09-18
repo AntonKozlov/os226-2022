dcba7f4e08835a80e95bed715719a47736479237
Polina Badreeva <badreeva.2003@gmail.com>
2022-09-18 09:23:09 +0300

    HW2, Polina Badreeva

--- pool.c
+++ pool.c
@@ -3,12 +3,30 @@
 
 #include "pool.h"
 
-void pool_init(struct pool *p, void *mem, unsigned long nmemb, unsigned long membsz) {
+void pool_init(struct pool* p, void* mem, unsigned long nmemb, unsigned long membsz) {
+	p->mem = mem;
+	p->membsz = membsz;
+	p->free_start = mem;
+	p->free_end = p->mem + membsz * nmemb;
+	p->free = NULL;
 }
 
-void *pool_alloc(struct pool *p) {
+void* pool_alloc(struct pool* p) {
+	struct free_block* fb = p->free;
+	if (fb) {
+		p->free = fb->next;
+		return fb;
+	}
+	if (p->free_start < p->free_end) {
+		void* res = p->free_start;
+		p->free_start += p->membsz;
+		return res;
+	}
 	return NULL;
 }
 
-void pool_free(struct pool *p, void *ptr) {
+void pool_free(struct pool* p, void* ptr) {
+	struct free_block* f = ptr;
+	f->next = p->free;
+	p->free = f;
 }
--- pool.h
+++ pool.h
@@ -3,17 +3,29 @@
 #include "util.h"
 
 struct pool {
+	char* mem;
+	unsigned long membsz;
+	char* free_start;
+	char* free_end;
+	struct free_block {
+		struct free_block* next;
+	} *free;
 
 };
 
 #define POOL_INITIALIZER(_mem, _nmemb, _membsz) { \
-}
+_mem,\
+_membsz,\
+_mem,\
+_mem + _membsz * _nmemb,\
+NULL\
+};
 
 #define POOL_INITIALIZER_ARRAY(_array) \
 	POOL_INITIALIZER(_array, ARRAY_SIZE(_array), sizeof((_array)[0]));
 
-void pool_init(struct pool *p, void *mem, unsigned long nmemb, unsigned long membsz);
+void pool_init(struct pool* p, void* mem, unsigned long nmemb, unsigned long membsz);
 
-void *pool_alloc(struct pool *p);
+void* pool_alloc(struct pool* p);
 
-void pool_free(struct pool *p, void *ptr);
+void pool_free(struct pool* p, void* ptr);
