1dc8d500cbda91328ce32cda106a8d392a7a398c
Ilya Pankratov <i.pankratov.main@gmail.com>
2022-09-18 19:28:39 +0300

    Fixed bug in 'POOL_INITIALIZER' at pool.h and update code style

--- main.c
+++ main.c
@@ -22,29 +22,29 @@ APPS_X(DECLARE)
 #undef DECLARE
 
 static const struct app {
-	const char* name;
-	int (*fn)(int, char* []);
+	const char *name;
+	int (*fn)(int, char *[]);
 } app_list[] = {
 #define ELEM(X) {#X, X},
 	APPS_X(ELEM)
 #undef ELEM
 };
 
-static int echo(int argc, char* argv[]) {
+static int echo(int argc, char *argv[]) {
 	for (int i = 1; i < argc; ++i) {
 		printf("%s%c", argv[i], i == argc - 1 ? '\n' : ' ');
 	}
 	return argc - 1;
 }
 
-static int retcode(int argc, char* argv[]) {
+static int retcode(int argc, char *argv[]) {
 	printf("%d\n", _retcode);
 	return 0;
 }
 
-static char** parse(char* input, int* output_argument_number) {
-	char* command_separators = "\n;";
-	char** parsed_input = (char**)malloc(sizeof(char*) * STANDARD_BUFFER);
+static char **parse(char *input, int *output_argument_number) {
+	char *command_separators = "\n;";
+	char **parsed_input = (char **)malloc(sizeof(char *) * STANDARD_BUFFER);
 	int tokens_found = 0;
 
 	parsed_input[tokens_found++] = strtok(input, command_separators);
@@ -58,8 +58,8 @@ static char** parse(char* input, int* output_argument_number) {
 	return parsed_input;
 }
 
-static int exec(int argc, char** argv) {
-	const struct app* app = NULL;
+static int exec(int argc, char **argv) {
+	const struct app *app = NULL;
 	for (int i = 0; i < ARRAY_SIZE(app_list); ++i) {
 		if (!strcmp(argv[0], app_list[i].name)) {
 			app = &app_list[i];
@@ -76,12 +76,12 @@ static int exec(int argc, char** argv) {
 	return _retcode;
 }
 
-static int interprete(int argc, char** argv) {
+static int interprete(int argc, char **argv) {
 	int arguments_count = 0;
-	char* arguments[STANDARD_BUFFER];
+	char *arguments[STANDARD_BUFFER];
 
 	for (int i = 0; i < argc; i++) {
-		char* arguments_separators = " ";
+		char *arguments_separators = " ";
 
 		arguments[arguments_count++] = strtok(argv[i], arguments_separators);
 
@@ -98,16 +98,15 @@ static int interprete(int argc, char** argv) {
 	return 0;
 }
 
-static int pooltest(int argc, char* argv[]) {
+static int pooltest(int argc, char *argv[]) {
 	struct obj {
-		void* field1;
-		void* field2;
+		void *field1;
+		void *field2;
 	};
 	static struct obj objmem[4];
 	static struct pool objpool = POOL_INITIALIZER_ARRAY(objmem);
-
 	if (!strcmp(argv[1], "alloc")) {
-		struct obj* o = pool_alloc(&objpool);
+		struct obj *o = pool_alloc(&objpool);
 		printf("alloc %ld\n", o ? (o - objmem) : -1);
 		return 0;
 	} else if (!strcmp(argv[1], "free")) {
@@ -118,11 +117,11 @@ static int pooltest(int argc, char* argv[]) {
 	}
 }
 
-int process_input(int argc, char* argv[]) {
+int process_input(int argc, char *argv[]) {
 	char input[STANDARD_INPUT_SIZE];
 	int argumentNumber = 0;
 	while (fgets(input, STANDARD_INPUT_SIZE, stdin) != NULL) {
-		char** parsed_input = parse(input, &argumentNumber);
+		char **parsed_input = parse(input, &argumentNumber);
 		interprete(argumentNumber, parsed_input);
 		free(parsed_input);
 	}
@@ -130,6 +129,6 @@ int process_input(int argc, char* argv[]) {
 	return 0;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 	return process_input(0, NULL);
 }
--- pool.c
+++ pool.c
@@ -3,29 +3,29 @@
 
 #include "pool.h"
 
-void pool_init(struct pool* p, void* mem, unsigned long nmemb, unsigned long membsz) {
-	p->untraced_memory = (char*)mem;
+void pool_init(struct pool *p, void *mem, unsigned long nmemb, unsigned long membsz) {
+	p->untraced_memory = (char *)mem;
 	p->block_size = membsz;
 	p->untraced_block_number = nmemb;
 	p->free = NULL;
 }
 
-void* pool_alloc(struct pool* p) {
+void *pool_alloc(struct pool *p) {
 	if (p->untraced_block_number > 0) {
 		p->untraced_block_number--;
-		void* ref_to_user_mem = p->untraced_memory;
+		void *ref_to_user_mem = p->untraced_memory;
 		p->untraced_memory = p->untraced_memory + p->block_size;
 		return ref_to_user_mem;
 	} else if (p->free) {
-		void* ref_to_user_mem = p->free;
-		p->free = ((struct pool_free_block*)p->free)->next;
+		void *ref_to_user_mem = p->free;
+		p->free = ((struct pool_free_block *)p->free)->next;
 		return ref_to_user_mem;
 	} else {
 		return NULL;
 	}
 }
 
-void pool_free(struct pool* p, void* ptr) {
-	((struct pool_free_block*)ptr)->next = p->free;
+void pool_free(struct pool *p, void *ptr) {
+	((struct pool_free_block *)ptr)->next = p->free;
 	p->free = ptr;
 }
