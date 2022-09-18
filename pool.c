bd1fea7fde71843c71fe4081ede63ec96ef4dd78
Anton Kraev <antonkraeww@gmail.com>
2022-09-18 22:47:44 +0300

    Pool allocator implementation

--- pool.c
+++ pool.c
@@ -1,14 +1,36 @@
-
 #include <stddef.h>
 
 #include "pool.h"
 
 void pool_init(struct pool *p, void *mem, unsigned long nmemb, unsigned long membsz) {
+	p->mem = mem;
+	p->membsz = membsz;
+	p->freestart = (char*)mem;
+	p->freeend = (char*)mem + nmemb * membsz;
+	p->free = NULL;
 }
 
 void *pool_alloc(struct pool *p) {
+    struct pool_free_block* fb = p->free;
+
+    if (fb) {
+        p->free = fb->next;
+        return fb;
+    }
+
+    if (p->freestart < p->freeend) {
+        void* r = p->freestart;
+        p->freestart += p->membsz;
+        return r;
+    }
+
 	return NULL;
 }
 
 void pool_free(struct pool *p, void *ptr) {
+    if (ptr == NULL) return;
+
+    struct pool_free_block* fb = ptr;
+    fb->next = p->free;
+    p->free = ptr;
 }
