96e569917ba453bceda1c26e149350da195b8ed1
Oleg Bogomolov <smmac072@gmail.com>
2022-09-19 05:29:01 +0300

    HW2

--- main.c
+++ main.c
@@ -1,75 +1,126 @@
 
-#include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-#define MAX_INPUT_SIZE 512
-#define MAX_ARGUMENT_SIZE 128
-#define MAX_ARGUMENT_COUNT 32
+#include "pool.h"
+
+static int g_retcode;
+
+#define APPS_X(X) \
+        X(echo) \
+        X(retcode) \
+        X(pooltest) \
 
 
-const char CONST_RETCODE_KEYWORD[] = "retcode";
-const char CONST_ECHO_KEYWORD[] = "echo";
-const char CONST_PARSE_DELIMETERS[] = " ;\n";
-int LASTRETCODE = 0;
+#define DECLARE(X) static int X(int, char *[]);
+APPS_X(DECLARE)
+#undef DECLARE
 
-int echo(int argc, char *argv[]) 
+static const struct app {
+        const char *name;
+        int (*fn)(int, char *[]);
+} app_list[] = {
+#define ELEM(X) { # X, X },
+        APPS_X(ELEM)
+#undef ELEM
+};
+
+static int echo(int argc, char *argv[]) 
 {
 	for (int i = 1; i < argc; ++i)
 		printf("%s%c", argv[i], i == argc - 1 ? '\n' : ' ');
 	return argc - 1;
 }
 
-int retcode() 
+static int retcode(int argc, char *argv[]) 
 {
-	printf("%d\n", LASTRETCODE);
+	printf("%d\n", g_retcode);
 	return 0;
 }
 
-void parse(char* InputString)
+static int exec(int argc, char *argv[]) 
 {
-	char* ProcessedPart = strtok(InputString, CONST_PARSE_DELIMETERS);
-	int Elements = 0;
-	char SplitInput[MAX_ARGUMENT_COUNT][MAX_ARGUMENT_SIZE];
-	while (ProcessedPart) 
+	const struct app *app = NULL;
+	for (int i = 0; i < ARRAY_SIZE(app_list); ++i) 
 	{
-		strcpy(SplitInput[Elements++], ProcessedPart);
-		ProcessedPart = strtok(NULL, CONST_PARSE_DELIMETERS);
-	}
-
-	for (int i = 0; i < Elements; i++)
-	{
-		if (!strcmp(SplitInput[i], CONST_RETCODE_KEYWORD))
-			LASTRETCODE = retcode();
-		if (!strcmp(SplitInput[i], CONST_ECHO_KEYWORD))
+		if (!strcmp(argv[0], app_list[i].name)) 
 		{
-			int argc = 1;
-			char *argv[MAX_ARGUMENT_COUNT];
-			for (int alloc = 1; alloc < MAX_ARGUMENT_COUNT; alloc++)
-				argv[alloc] = (char*)malloc(MAX_ARGUMENT_SIZE);
-
-			for (int j = i + 1; j < Elements; j++)
-			{
-				if (!strcmp(SplitInput[j], CONST_ECHO_KEYWORD) || !strcmp(SplitInput[j], CONST_RETCODE_KEYWORD))
-					break;
-				else 
-					strcpy(argv[argc++], SplitInput[j]);
-			}
-			LASTRETCODE = echo(argc, argv);
-			
-			for (int alloc = 1; alloc < MAX_ARGUMENT_COUNT; alloc++)
-				free(argv[alloc]);
+			app = &app_list[i];
+			break;
 		}
 	}
-	free(ProcessedPart);
+
+	if (!app) 
+	{
+		printf("Unknown command\n");
+		return 1;
+	}
+
+	g_retcode = app->fn(argc, argv);
+	return g_retcode;
 }
+
+static int pooltest(int argc, char *argv[]) 
+{
+	struct obj 
+	{
+		void *field1;
+		void *field2;
+	};
+	static struct obj objmem[4];
+	static struct pool objpool = POOL_INITIALIZER_ARRAY(objmem);
+
+	if (!strcmp(argv[1], "alloc")) 
+	{
+		struct obj *o = pool_alloc(&objpool);
+		printf("alloc %d\n", o ? (o - objmem) : -1);
+		return 0;
+	} 
+	else if (!strcmp(argv[1], "free")) 
+	{
+		int iobj = atoi(argv[2]);
+		printf("free %d\n", iobj);
+		pool_free(&objpool, objmem + iobj);
+		return 0;
+	}
+}
+
+int shell(int argc, char *argv[]) 
+{
+	char line[256];
+	while (fgets(line, sizeof(line), stdin)) 
+	{
+		const char *comsep = "\n;";
+		char *stcmd;
+		char *cmd = strtok_r(line, comsep, &stcmd);
+		while (cmd) {
+			const char *argsep = " ";
+			char *starg;
+			char *arg = strtok_r(cmd, argsep, &starg);
+			char *argv[256];
+			int argc = 0;
+			while (arg) 
+			{
+				argv[argc++] = arg;
+				arg = strtok_r(NULL, argsep, &starg);
+			}
+			argv[argc] = NULL;
+
+			if (!argc)
+				break;
+
+			exec(argc, argv);
+
+			cmd = strtok_r(NULL, comsep, &stcmd);
+		}
+	}
+	return 0;
+}
+
 
 int main(int argc, char *argv[]) 
 {
-	char InputString[MAX_INPUT_SIZE];
-	while (fgets(InputString, MAX_INPUT_SIZE, stdin))
-	{
-		parse(InputString);
-	}
-	return 0;
+	shell(0, NULL);
 }
