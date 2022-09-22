85b005bb6730ac24b5dcc326f4c3ef134b4ca1c1
Daria Goldenberg <dasha@igold.pp.ru>
2022-09-23 00:21:59 +0300

    Added solution for third homework.

--- kernel.c
+++ kernel.c
@@ -11,23 +11,27 @@
 
 extern int shell(int argc, char *argv[]);
 
-static void sighnd(int sig, siginfo_t *info, void *ctx) {
-	ucontext_t *uc = (ucontext_t *) ctx;
+static void sighnd(int sig, siginfo_t *info, void *ctx)
+{
+	ucontext_t *uc = (ucontext_t *)ctx;
 	greg_t *regs = uc->uc_mcontext.gregs;
 
-	syscall_do(regs[REG_RAX], regs[REG_RBX],
-			regs[REG_RCX], regs[REG_RDX],
-			regs[REG_RSI], (void *) regs[REG_RDI]);
+	regs[REG_RAX] = (greg_t)syscall_do(regs[REG_RAX], regs[REG_RBX],
+									   regs[REG_RCX], regs[REG_RDX],
+									   regs[REG_RSI], (void *)regs[REG_RDI]);
+	regs[REG_RIP] += 2;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	struct sigaction act = {
 		.sa_sigaction = sighnd,
 		.sa_flags = SA_RESTART,
 	};
 	sigemptyset(&act.sa_mask);
 
-	if (-1 == sigaction(SIGSEGV, &act, NULL)) {
+	if (-1 == sigaction(SIGSEGV, &act, NULL))
+	{
 		perror("signal set failed");
 		return 1;
 	}
