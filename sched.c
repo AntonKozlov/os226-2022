105c1b1ecfc4d24083d2f0eaa114392a06282843
Startcev <tozarin@mail.ru>
2022-10-12 21:07:47 +0300

    Implemented sched_new

--- sched.c
+++ sched.c
@@ -14,6 +14,26 @@ void sched_new(void (*entrypoint)(void *aspace),
 		int priority,
 		int deadline) {
 
+		if (!tasks) {
+			tasks = (task*)malloc(sizeof(task));
+			tasks->task_link.next = tasks;
+			tasks->task_link.prev = tasks;
+			tasks->entrypoint = NULL;
+			tasks->ctx = NULL;
+			tasks->priority = 0;
+			tasks->deadline = 0;
+			tasks->time_when_can_start = -1;
+		}
+
+		task* last = ((task_link*)tasks)->prev;
+		task* new = (task*)malloc(sizeof(task));
+		((task_link*)new)->next = tasks;
+		((task_link*)new)->prev = last;
+		new->entrypoint = entrypoint;
+		new->ctx = aspace;
+		new->priority = priority;
+		new->deadline = deadline;
+		new->time_when_can_start = -1;
 }
 
 void sched_cont(void (*entrypoint)(void *aspace),
