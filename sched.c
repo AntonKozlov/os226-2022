1489864b2ad006a7554730acee6c20a340c74473
VladYoSlav <vaifaer@gmail.com>
2022-10-17 05:32:46 +0300

    feat: schedule

--- sched.c
+++ sched.c
@@ -5,25 +5,129 @@
 #include "sched.h"
 #include "pool.h"
 
-static int time;
+static int time = 0;
 
-void sched_new(void (*entrypoint)(void *aspace),
-		void *aspace,
-		int priority,
-		int deadline) {
+static task_t task_array[10];
+static struct pool task_pool = POOL_INITIALIZER_ARRAY(task_array)
 
+static task_list_t task_list = {NULL};
+
+static task_t *current_task;
+
+void add_task(
+        entrypoint_t entrypoint,
+        void *ctx,
+        int priority,
+        int deadline,
+        int timeout
+) {
+    task_t *new_task = pool_alloc(&task_pool);
+    new_task->entrypoint = entrypoint;
+    new_task->ctx = ctx;
+    new_task->priority = priority;
+    new_task->deadline = deadline;
+    new_task->timeout = timeout;
+    new_task->created_at = time;
+    new_task->next = task_list.first;
+    task_list.first = new_task;
 }
 
-void sched_cont(void (*entrypoint)(void *aspace),
-		void *aspace,
-		int timeout) {
+void sched_new(
+        entrypoint_t entrypoint,
+        void *ctx,
+        int priority,
+        int deadline) {
 
+    add_task(entrypoint, ctx, priority, deadline, 0);
+}
+
+void sched_cont(
+        entrypoint_t entrypoint,
+        void *ctx,
+        int timeout) {
+    if (current_task != NULL)
+        add_task(entrypoint, ctx, current_task->priority, current_task->deadline, timeout);
 }
 
 void sched_time_elapsed(unsigned amount) {
-	time += amount;
+    time += amount;
+}
 
+task_t *choose_by_fifo(task_t *first, task_t *second) {
+    return second;
+}
+
+task_t *choose_by_prio(task_t *first, task_t *second) {
+    return first->priority > second->priority ? first : second;
+}
+
+task_t *choose_by_deadline(task_t *first, task_t *second) {
+    if (first->deadline == second->deadline)
+        return choose_by_prio(first, second);
+
+    if (first->deadline <= 0)
+        return second;
+    else if (second->deadline <= 0)
+        return first;
+
+    return first->deadline < second->deadline ? first : second;
+}
+
+policy_fn_t *choose_policy_fn(enum policy policy) {
+    switch (policy) {
+        case POLICY_FIFO:
+            return choose_by_fifo;
+        case POLICY_PRIO:
+            return choose_by_prio;
+        case POLICY_DEADLINE:
+            return choose_by_deadline;
+        default:
+            return NULL;
+    }
 }
 
 void sched_run(enum policy policy) {
+    policy_fn_t *policy_fn = choose_policy_fn(policy);
+
+    while (task_list.first != NULL) {
+        task_t *chosen = task_list.first;
+        task_t *next = chosen->next;
+
+        task_t *test = task_list.first;
+        while (test != NULL) {
+            test = test->next;
+        }
+
+
+        while (next != NULL) {
+            if (chosen->created_at + chosen->timeout > time)
+                chosen = next;
+            else if (next->created_at + next->timeout <= time)
+                chosen = policy_fn(chosen, next);
+
+            next = next->next;
+        }
+
+        if (chosen->created_at + chosen->timeout <= time) {
+            current_task = chosen;
+
+            current_task->entrypoint(current_task->ctx);
+
+            if (current_task == task_list.first)
+                task_list.first = current_task->next;
+            else {
+                task_t *prev = task_list.first;
+
+                while (current_task != prev->next)
+                    prev = prev->next;
+
+                prev->next = current_task->next;
+            }
+
+            pool_free(&task_pool, current_task);
+        } else {
+            sched_time_elapsed(1);
+        }
+
+    }
 }
--- sched.h
+++ sched.h
@@ -1,29 +1,48 @@
 #pragma once
 
 enum policy {
-	// first-in, first-out; run tasks in order of their arrival
-	POLICY_FIFO,
+    // first-in, first-out; run tasks in order of their arrival
+    POLICY_FIFO,
 
-	// highest priority task (highest priority value) should be executed
-	// first. Use round-robin for processes with same priority
-	// (task from 1st process, from 2nd, ... Nth, 1st, 2nd, ...)
-	POLICY_PRIO,
+    // highest priority task (highest priority value) should be executed
+    // first. Use round-robin for processes with same priority
+    // (task from 1st process, from 2nd, ... Nth, 1st, 2nd, ...)
+    POLICY_PRIO,
 
-	// consider deadline, execute process with Earliest Deadline First.
-	// Fallback to priority policy if deadlines are equal
-	POLICY_DEADLINE,
+    // consider deadline, execute process with Earliest Deadline First.
+    // Fallback to priority policy if deadlines are equal
+    POLICY_DEADLINE,
 };
 
+typedef struct tsk {
+    void (*entrypoint)(void *);
+
+    void *ctx;
+    int priority;
+    int deadline;
+    int created_at;
+    int timeout;
+    struct tsk *next;
+} task_t;
+
+typedef struct tsk_lst {
+    task_t *first;
+} task_list_t;
+
+typedef task_t *(policy_fn_t)(task_t *, task_t *);
+
+typedef void (*entrypoint_t)(void *);
+
 // Add new task
-extern void sched_new(void (*entrypoint)(void *ctx), // entrypoint function
-		void *ctx,     // context of the process
-		int priority,  // priority, [0 - 10], bigger for more priority
-		int deadline); // absolute time till the task should be completed, <=0 for no deadline
+extern void sched_new(entrypoint_t entrypoint, // entrypoint function
+                      void *ctx,     // context of the process
+                      int priority,  // priority, [0 - 10], bigger for more priority
+                      int deadline); // absolute time till the task should be completed, <=0 for no deadline
 
 // Continue process from function after some amount of time
-extern void sched_cont(void (*entrypoint)(void *aspace), // entrypoint function
-		void *aspace,// addresses the process can access
-		int timeout); // when the continuation became runnable
+extern void sched_cont(entrypoint_t entrypoint, // entrypoint function
+                       void *aspace,// addresses the process can access
+                       int timeout); // when the continuation became runnable
 
 // Notify scheduler that some amount of time passed
 extern void sched_time_elapsed(unsigned amount);
