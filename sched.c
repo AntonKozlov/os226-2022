21c70488188aa1d9ffcd6bc2d87606a2e5482927
ArtemKhel <ArtemKhel@gmail.com>
2022-10-17 06:25:44 +0300

    HW 4

--- sched.c
+++ sched.c
@@ -1,29 +1,150 @@
 #include <limits.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "sched.h"
 #include "pool.h"
 
-static int time;
+static int time = 0;
+static task *tasks = NULL;
+static task *current_task = NULL;
+
+void delete_task(task *task_) {
+    task *curr = tasks;
+    if (curr == task_) {
+        tasks = curr->next;
+        free(curr);
+        return;
+    }
+    task *prev = NULL;
+    while (curr->next && curr != task_) {
+        prev = curr;
+        curr = curr->next;
+    }
+    if (curr == task_) {
+        prev->next = curr->next;
+        free(curr);
+    }
+}
+
+void policy_fifo() {
+    while (tasks) {
+        current_task = tasks;
+        while (current_task) {
+            if (current_task->timeout <= time) {
+                current_task->entrypoint(current_task->context);
+                task *done = current_task;
+                current_task = current_task->next;
+                delete_task(done);
+            }
+        }
+    }
+}
+
+task *max_priority() {
+    if (!tasks) {
+        return NULL;
+    }
+    task *curr = tasks;
+    task *best = NULL;
+    while (curr) {
+        if ((!best || curr->priority > best->priority) && curr->timeout <= time) {
+            best = curr;
+        }
+        curr = curr->next;
+    }
+    return best;
+}
+
+void policy_prio() {
+    current_task = max_priority();
+    while (current_task) {
+        current_task->entrypoint(current_task->context);
+        delete_task(current_task);
+        current_task = max_priority();
+    }
+}
+
+task *min_deadline() {
+    task *curr, *best;
+    int min_deadline = INT_MAX;
+    curr = tasks;
+    best = NULL;
+    while (curr) {
+        if (curr->deadline >= 0 && curr->deadline < min_deadline && curr->timeout <= time) {
+            best = curr;
+            min_deadline = best->deadline;
+        }
+        curr = curr->next;
+    }
+    if (best) {
+        return best;
+    } else {
+        return max_priority();
+    }
+}
+
+void policy_deadline() {
+    current_task = min_deadline();
+    while (current_task) {
+        current_task->entrypoint(current_task->context);
+        delete_task(current_task);
+        current_task = min_deadline();
+    }
+    if (tasks) {
+        policy_prio();
+    }
+}
 
 void sched_new(void (*entrypoint)(void *aspace),
-		void *aspace,
-		int priority,
-		int deadline) {
-
+               void *aspace,
+               int priority,
+               int deadline) {
+    task *new_task = (task *) malloc(sizeof(task));
+    new_task->next = NULL;
+    new_task->entrypoint = entrypoint;
+    new_task->context = aspace;
+    new_task->priority = priority;
+    new_task->deadline = deadline;
+    new_task->timeout = -1;
+    if (tasks) {
+        task *curr = tasks;
+        while (curr->next) {
+            curr = curr->next;
+        }
+        curr->next = new_task;
+    } else {
+        tasks = new_task;
+    }
 }
 
 void sched_cont(void (*entrypoint)(void *aspace),
-		void *aspace,
-		int timeout) {
-
+                void *aspace,
+                int timeout) {
+    sched_new(entrypoint, aspace, current_task->priority, current_task->deadline);
+    task *curr = tasks;
+    while (curr->next) {
+        curr = curr->next;
+    }
+    curr->timeout = timeout + time;
 }
 
 void sched_time_elapsed(unsigned amount) {
-	time += amount;
-
+    time += amount;
 }
 
 void sched_run(enum policy policy) {
+    if (!tasks) {
+        return;
+    }
+    if (policy == POLICY_FIFO) {
+        policy_fifo();
+    } else if (policy == POLICY_PRIO) {
+        policy_prio();
+    } else if (policy == POLICY_DEADLINE) {
+        policy_deadline();
+    }
+
 }
+
--- sched.h
+++ sched.h
@@ -1,29 +1,38 @@
 #pragma once
 
+typedef struct task {
+    struct task *next;
+    void (*entrypoint)(void *context);
+    void *context;
+    int priority;
+    int deadline;
+    int timeout;
+} task;
+
 enum policy {
-	// first-in, first-out; run tasks in order of their arrival
-	POLICY_FIFO,
+    // first-in, first-out; run tasks in order of their arrival
+    POLICY_FIFO,
 
-	// highest priority task (highest priority value) should be executed
-	// first. Use round-robin for processes with same priority
-	// (task from 1st process, from 2nd, ... Nth, 1st, 2nd, ...)
-	POLICY_PRIO,
+    // highest priority task (highest priority value) should be executed
+    // first. Use round-robin for processes with same priority
+    // (task from 1st process, from 2nd, ... Nth, 1st, 2nd, ...)
+    POLICY_PRIO,
 
-	// consider deadline, execute process with Earliest Deadline First.
-	// Fallback to priority policy if deadlines are equal
-	POLICY_DEADLINE,
+    // consider deadline, execute process with Earliest Deadline First.
+    // Fallback to priority policy if deadlines are equal
+    POLICY_DEADLINE,
 };
 
 // Add new task
 extern void sched_new(void (*entrypoint)(void *ctx), // entrypoint function
-		void *ctx,     // context of the process
-		int priority,  // priority, [0 - 10], bigger for more priority
-		int deadline); // absolute time till the task should be completed, <=0 for no deadline
+                      void *ctx,     // context of the process
+                      int priority,  // priority, [0 - 10], bigger for more priority
+                      int deadline); // absolute time till the task should be completed, <=0 for no deadline
 
 // Continue process from function after some amount of time
 extern void sched_cont(void (*entrypoint)(void *aspace), // entrypoint function
-		void *aspace,// addresses the process can access
-		int timeout); // when the continuation became runnable
+                       void *aspace,// addresses the process can access
+                       int timeout); // when the continuation became runnable
 
 // Notify scheduler that some amount of time passed
 extern void sched_time_elapsed(unsigned amount);
