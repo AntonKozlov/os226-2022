88185bf5595b220671c9bc9e5054c103d01cd6db
Startsev <tozarin@mail.ru>
2022-10-20 20:31:23 +0300

    Implemented timer_cnt and timer_init

--- timer.c
+++ timer.c
@@ -11,9 +11,16 @@
 #include "timer.h"
 
 int timer_cnt(void) {
-	// TODO: getitimer
+	struct itimerval t;
+	if (getitimer(ITIMER_REAL, &t)) {
+		return -1;
+	}
+
+	return (t.it_interval.tv_sec - t.it_value.tv_sec) * 1000000 + t.it_interval.tv_usec - t.it_value.tv_usec;
 }
 
 void timer_init(int ms, void (*hnd)(void)) {
-	// TODO: setitimer
+	struct timeval interval = {0, ms * 1000};
+	struct itimerval t = {interval, interval};
+	setitimer(ITIMER_REAL, &t, NULL);
 }
