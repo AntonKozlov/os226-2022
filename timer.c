93a10ab916df7ab24e45431d165987dcb408b27d
Max Drumov <Drumov.M.A@gmail.com>
2022-10-24 03:05:03 +0300

    Implement timer_cnt, timer_init. Change sched.c

--- timer.c
+++ timer.c
@@ -10,10 +10,27 @@
 
 #include "timer.h"
 
-int timer_cnt(void) {
-	// TODO: getitimer
+int timer_cnt(void)
+{
+	struct itimerval timer;
+	getitimer(ITIMER_REAL, &timer);
+
+	long interval = timer.it_interval.tv_sec * 1000 + timer.it_interval.tv_usec / 1000;
+	long time_left = timer.it_value.tv_sec * 1000 + timer.it_value.tv_usec / 1000;
+
+	return (interval - time_left);
 }
 
-void timer_init(int ms, void (*hnd)(void)) {
-	// TODO: setitimer
+void timer_init(int ms, void (*hnd)(void))
+{
+	struct timeval interval = {
+		.tv_sec = ms / 1000,
+		.tv_usec = ms * 1000};
+
+	struct itimerval new_value = {
+		.it_interval = interval,
+		.it_value = interval};
+
+	setitimer(ITIMER_REAL, &new_value, NULL);
+	signal(SIGALRM, hnd);
 }
