e0b0fc8cd8f8f0855f59332ecc78d1923c2a4dee
Startsev <tozarin@mail.ru>
2022-10-30 17:02:07 +0300

    Returned to old implementation

--- sched.c
+++ sched.c
@@ -84,6 +84,7 @@ static void tasktramp(void) {
         irq_enable();
         current->entry(current->as);
         irq_disable();
+        doswitch();
 }
 
 void sched_new(void (*entrypoint)(void *aspace),
@@ -150,17 +151,19 @@ static void hctx_push(greg_t *regs, unsigned long val) {
 static void bottom(void) {
         time += TICK_PERIOD;
 
-    irq_disable();
-    policy_run(current);
-    int cur_time = sched_gettime();
-    struct task **c = &waitq;
-    while (*c && (*c)->waketime <= cur_time) {
-        policy_run(*c);
-        c = &(*c)->next;
-    }
+        while (waitq && waitq->waketime <= sched_gettime()) {
+		struct task *t = waitq;
+		waitq = waitq->next;
+		policy_run(t);
+	}
 
-    doswitch();
-    irq_enable();
+
+	if (TICK_PERIOD <= sched_gettime() - current_start) {
+		irq_disable();
+		policy_run(current);
+		doswitch();
+		irq_enable();
+	}
 }
 
 static void top(int sig, siginfo_t *info, void *ctx) {
