93c91e7eefc8a8249ab98825a2c666c0ca36d83e
Max Drumov <Drumov.M.A@gmail.com>
2022-10-31 00:06:56 +0300

    HW 6

--- sched.c
+++ sched.c
@@ -20,7 +20,8 @@ be reserved and shall not be modified by signal or interrupt handlers */
 
 extern void tramptramp(void);
 
-struct task {
+struct task
+{
 	char stack[8192];
 	struct ctx ctx;
 
@@ -53,40 +54,50 @@ static struct pool taskpool = POOL_INITIALIZER_ARRAY(taskarray);
 
 static sigset_t irqs;
 
-void irq_disable(void) {
-        sigprocmask(SIG_BLOCK, &irqs, NULL);
+void irq_disable(void)
+{
+	sigprocmask(SIG_BLOCK, &irqs, NULL);
 }
 
-void irq_enable(void) {
-        sigprocmask(SIG_UNBLOCK, &irqs, NULL);
+void irq_enable(void)
+{
+	sigprocmask(SIG_UNBLOCK, &irqs, NULL);
 }
 
 static void policy_run(struct task *t)
 {
 	struct task **c = &runq;
 
-	while (*c && (t == idle || policy_cmp(*c, t) <= 0)) {
+	while (*c && (t == idle || policy_cmp(*c, t) <= 0))
+	{
 		c = &(*c)->next;
 	}
 	t->next = *c;
 	*c = t;
 }
 
-static void doswitch(void) {
-        struct task *old = current;
-        current = runq;
-        runq = current->next;
+static void doswitch(void)
+{
+	struct task *old = current;
+	current = runq;
+	runq = current->next;
 
-        current_start = sched_gettime();
-        ctx_switch(&old->ctx, &current->ctx);
+	current_start = sched_gettime();
+	ctx_switch(&old->ctx, &current->ctx);
 }
 
-static void tasktramp(void) {
+static void tasktramp(void)
+{
+	irq_enable();
+	current->entry(current->as);
+	irq_disable();
+	doswitch();
 }
 
 void sched_new(void (*entrypoint)(void *aspace),
-		void *aspace,
-		int priority) {
+			   void *aspace,
+			   int priority)
+{
 
 	struct task *t = pool_alloc(&taskpool);
 	t->entry = entrypoint;
@@ -96,7 +107,8 @@ void sched_new(void (*entrypoint)(void *aspace),
 
 	ctx_make(&t->ctx, tasktramp, t->stack, sizeof(t->stack));
 
-	if (!lastpending) {
+	if (!lastpending)
+	{
 		lastpending = t;
 		pendingq = t;
 	}
@@ -107,31 +119,35 @@ void sched_new(void (*entrypoint)(void *aspace),
 	}
 }
 
-void sched_sleep(unsigned ms) {
+void sched_sleep(unsigned ms)
+{
 
-        if (!ms) {
-                irq_disable();
-                policy_run(current);
-                doswitch();
-                irq_enable();
-                return;
-        }
+	if (!ms)
+	{
+		irq_disable();
+		policy_run(current);
+		doswitch();
+		irq_enable();
+		return;
+	}
 
-        current->waketime = sched_gettime() + ms;
+	current->waketime = sched_gettime() + ms;
 
-        int curtime;
-        while ((curtime = sched_gettime()) < current->waketime) {
-                irq_disable();
-                struct task **c = &waitq;
-                while (*c && (*c)->waketime < current->waketime) {
-                        c = &(*c)->next;
-                }
-                current->next = *c;
-                *c = current;
+	int curtime;
+	while ((curtime = sched_gettime()) < current->waketime)
+	{
+		irq_disable();
+		struct task **c = &waitq;
+		while (*c && (*c)->waketime < current->waketime)
+		{
+			c = &(*c)->next;
+		}
+		current->next = *c;
+		*c = current;
 
-                doswitch();
-                irq_enable();
-        }
+		doswitch();
+		irq_enable();
+	}
 }
 
 static int fifo_cmp(struct task *t1, struct task *t2)
@@ -144,42 +160,57 @@ static int prio_cmp(struct task *t1, struct task *t2)
 	return t2->priority - t1->priority;
 }
 
-static void hctx_push(greg_t *regs, unsigned long val) {
-        regs[REG_RSP] -= sizeof(unsigned long);
-        *(unsigned long *) regs[REG_RSP] = val;
+static void hctx_push(greg_t *regs, unsigned long val)
+{
+	regs[REG_RSP] -= sizeof(unsigned long);
+	*(unsigned long *)regs[REG_RSP] = val;
 }
 
-static void bottom(void) {
-        time += TICK_PERIOD;
+static void bottom(void)
+{
+	time += TICK_PERIOD;
+	irq_disable();
+
+	policy_run(current);
+	while (waitq != NULL && waitq->waketime <= sched_gettime())
+	{
+		struct task *task = waitq;
+		waitq = waitq->next;
+		policy_run(task);
+	}
+
+	doswitch();
+	irq_enable();
 }
 
-static void top(int sig, siginfo_t *info, void *ctx) {
-        ucontext_t *uc = (ucontext_t *) ctx;
-        greg_t *regs = uc->uc_mcontext.gregs;
+static void top(int sig, siginfo_t *info, void *ctx)
+{
+	ucontext_t *uc = (ucontext_t *)ctx;
+	greg_t *regs = uc->uc_mcontext.gregs;
 
-        unsigned long oldsp = regs[REG_RSP];
-        regs[REG_RSP] -= SYSV_REDST_SZ;
-        hctx_push(regs, regs[REG_RIP]);
-        hctx_push(regs, sig);
-        hctx_push(regs, regs[REG_RBP]);
-        hctx_push(regs, oldsp);
-        hctx_push(regs, (unsigned long) bottom);
-        regs[REG_RIP] = (greg_t) tramptramp;
+	unsigned long oldsp = regs[REG_RSP];
+	regs[REG_RSP] -= SYSV_REDST_SZ;
+	hctx_push(regs, regs[REG_RIP]);
+	hctx_push(regs, sig);
+	hctx_push(regs, regs[REG_RBP]);
+	hctx_push(regs, oldsp);
+	hctx_push(regs, (unsigned long)bottom);
+	regs[REG_RIP] = (greg_t)tramptramp;
 }
 
-long sched_gettime(void) {
-        int cnt1 = timer_cnt() / 1000;
-        int time1 = time;
-        int cnt2 = timer_cnt() / 1000;
-        int time2 = time;
+long sched_gettime(void)
+{
+	int cnt1 = timer_cnt() / 1000;
+	int time1 = time;
+	int cnt2 = timer_cnt() / 1000;
+	int time2 = time;
 
-        return (cnt1 <= cnt2) ?
-                time1 + cnt2 :
-                time2 + cnt2;
+	return (cnt1 <= cnt2) ? time1 + cnt2 : time2 + cnt2;
 }
 
-void sched_run(enum policy policy) {
-	int (*policies[])(struct task *t1, struct task *t2) = { fifo_cmp, prio_cmp };
+void sched_run(enum policy policy)
+{
+	int (*policies[])(struct task * t1, struct task * t2) = {fifo_cmp, prio_cmp};
 	policy_cmp = policies[policy];
 
 	struct task *t = pendingq;
@@ -204,16 +235,18 @@ void sched_run(enum policy policy) {
 	sigset_t none;
 	sigemptyset(&none);
 
-	while (runq || waitq) {
-		if (runq) {
-                        policy_run(current);
-                        doswitch();
-                } else {
-                        sigsuspend(&none);
-                }
-
+	while (runq || waitq)
+	{
+		if (runq)
+		{
+			policy_run(current);
+			doswitch();
+		}
+		else
+		{
+			sigsuspend(&none);
+		}
 	}
 
 	irq_enable();
 }
-
