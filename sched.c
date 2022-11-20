a04af8d9deb0f7bc6554cabf9043a3efbdce8236
Arthur Alekseev <57834711+arthur100500@users.noreply.github.com>
2022-11-21 01:32:19 +0300

    Fixed tabs

--- sched.c
+++ sched.c
@@ -402,8 +402,8 @@ static void exectramp(void) {
 
 static int do_exec(const char *path, char *argv[]) {
 	char elfpath[32];
-    strcpy(elfpath, path);
-    strcat(elfpath, ".app");
+	strcpy(elfpath, path);
+	strcat(elfpath, ".app");
 	int fd = open(elfpath, O_RDONLY);
 	if (fd < 0) {
 		perror("open");
