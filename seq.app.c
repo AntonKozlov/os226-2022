ebf34a21eff926af515182af470e7a050f614602
Startsev <tozarin@mail.ru>
2022-11-29 01:10:10 +0300

    Added missed file
    Signed-off-by: Startsev <tozarin@mail.ru>

--- seq.app.c
+++ seq.app.c
@@ -2,9 +2,45 @@
 
 
 int atoi(const char *str) {
+	int i = 0;
+	int rez = 0;
+	while (str[i] != '\0') {
+		rez += rez * 10 + str[i] - '0';
+	}
+
+	return rez;
 }
 
 int itoa(int v, char *d) {
+	char sign = v < 0;
+	int unsigv = v;
+	if (sign) {
+		unsigv *= -1;
+	}
+
+	char* buf[32];
+	char* cur = buf;
+
+	while (unsigv > 0) {
+		*cur = (char)(unsigv % 10 + '0');
+		cur++;
+		unsigv /= 10;
+	}
+
+	if (sign) {
+		*d = '-';
+		d++;
+	}
+	
+	int len = cur - buf;
+
+	while (cur > buf) {
+		*d = *cur;
+		d++;
+		cur--;
+	}
+
+	return len;
 }
 
 int main(int argc, char* argv[]) {
