ffd7393429eb7a161cc3dcfe13dc65560126dac1
Startsev Matvey <89902951+Tozarin@users.noreply.github.com>
2022-12-04 19:58:57 +0300

    Fixes snprintf?

--- sched.c
+++ sched.c
@@ -455,7 +455,8 @@ static void exectramp(void) {
 
 int sys_exec(const char *path, char **argv) {
 	char elfpath[32];
-	snprintf(elfpath, sizeof(elfpath), "%s.app", path);
+	strcpy(elfpath, path);
+	strcat(elfpath, ".app");
 	int fd = open(elfpath, O_RDONLY);
 	if (fd < 0) {
 		perror("open");
